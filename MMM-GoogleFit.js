aa2aa0f4fe18ffca17d30ef4e5a2e9e3db7442df
Andrew McOlash <amcolash@gmail.com>
2018-01-13 16:43:01 -0600

    Lots of work on the module, oauth stuff working, got bassic stats, etc

--- MMM-GoogleFit.js
+++ MMM-GoogleFit.js
@@ -2,10 +2,11 @@
 
 Module.register("MMM-GoogleFit", {
 
-  result: undefined,
+  auth: undefined,
   code: undefined,
   defaults: {
-    updateInterval: 1200000 // 20 minutes
+    updateInterval: 1800000, // 30 minutes
+    imperial: true
   },
 
   clientId: "846766038767-8fs63le8h45dhjpf0umhc1ai07q4rhn7.apps.googleusercontent.com",
@@ -23,11 +24,79 @@ Module.register("MMM-GoogleFit", {
     title.innerHTML = "Google Fit";
     wrapper.appendChild(title);
 
-    if (this.code) {
+    if (this.stats) {
+      for (var i = 0; i < this.stats.bucket.length; i++) {
+        var bucket = this.stats.bucket[i];
+        var elem = document.createElement("span");
+
+        for (var j = 0; j < bucket.dataset.length; j++) {
+          var data = bucket.dataset[j];
+
+          var start = new Date(Number.parseFloat(bucket.startTimeMillis)).toLocaleDateString();
+          var value = start;
+
+          var weight = false;
+          var steps = false;
+
+          if (data.dataSourceId.indexOf("weight") != -1) {
+            value += ", weight: ";
+            weight = true;
+          } else if (data.dataSourceId.indexOf("step_count") != -1) {
+            value += ", steps: ";
+            steps = true;
+          }
+
+          var total = 0;
+          for (var k = 0; k < data.point.length; k++) {
+            var point = data.point[k];
+
+            var tmp = 0;
+            for (var l = 0; l < point.value.length; l++) {
+              if (point.value[l].intVal) {
+                tmp += point.value[l].intVal;
+              } else if (point.value[l].fpVal) {
+                tmp += point.value[l].fpVal;
+              }
+            }
+
+            if (weight && point.value.length > 0) {
+              // Average weights
+              tmp /= point.value.length;
+            }
+
+            total += tmp;
+          }
+
+          if (weight) {
+            if (data.point.length > 0) {
+              total /= data.point.length;
+
+              if (this.config.imperial) {
+                total *= 2.20462;
+                total = total.toFixed(2);
+                total += " lbs";
+              } else {
+                total = total.toFixed(2);
+                total += " kg";
+              }
+            } else {
+              total = undefined;
+            }
+          }
+
+          value += total;
+
+          elem.innerHTML += value + ((j < bucket.dataset.length - 1) ? ", " : "");
+        }
+
+        elem.innerHTML += "<br>";
+        wrapper.appendChild(elem);
+      }
+    } else if (this.code && !this.auth) {
       var elem = document.createElement("span");
       elem.innerHTML = "Please Visit: " + this.code.verification_url + "<br>" + "Code: " + this.code.user_code;
       wrapper.appendChild(elem);
-    } else if (this.result) {
+    } else if (this.auth) {
       var elem = document.createElement("span");
       elem.innerHTML = "You are all authenticated";
       wrapper.appendChild(elem);
@@ -53,20 +122,20 @@ Module.register("MMM-GoogleFit", {
   },
 
   getStats: function () {
-    // this.sendSocketNotification("GET_STATS", this.config.url);
-    this.sendSocketNotification("GET_AUTH");
+    this.sendSocketNotification("UPDATE");
   },
 
   socketNotificationReceived: function(notification, result) {
-    this.code = undefined;
-    this.result = undefined;
+    // this.code = undefined;
+    // this.result = undefined;
 
-    if (notification === "AUTH_BODY") {
+    if (notification === "AUTH_CODE_BODY") {
       this.code = result;
-      // this.scheduleUpdate(5000);
-      this.getStats();
-    } else if (notification === "OAUTH_BODY") {
-      this.result = result;
+      console.log("code: " + result.user_code)
+    } else if (notification === "REFRESH_TOKEN_BODY") {
+      this.auth = result;
+    } else if (notification === "STATS") {
+      this.stats = result;
     }
 
     console.log(notification);
--- node_helper.js
+++ node_helper.js
@@ -1,55 +1,91 @@
-var NodeHelper = require("node_helper");
-var request = require("request");
+const NodeHelper = require("node_helper");
+const request = require("request");
+const jsonfile = require("jsonfile");
+const path = require("path");
+const dataFile = path.resolve(__dirname, "data.json");
 
 module.exports = NodeHelper.create({
-  device_code: undefined,
-  secret: undefined,
-  oauth: undefined,
+
+  client_id: "846766038767-8fs63le8h45dhjpf0umhc1ai07q4rhn7.apps.googleusercontent.com",
+  client_secret: "kbJlTr7uQPCiClb5JSrGE-Ve",
+  config: {
+    //device_code,
+    //refresh_token
+  },
+
+  tmpAuthData: undefined,
+  tmpAccessToken: undefined,
 
   start: function () {
     console.log("MMM-GoogleFit helper started...");
+
+    try {
+      var c = jsonfile.readFileSync(dataFile);
+      if (c.refresh_token) {
+        this.config = c;
+      }
+    } catch (error) {
+      // Probably have not yet written the file, no worries
+    }
   },
 
   getAuthCode: function() {
     var self = this;
 
     var url = "https://accounts.google.com/o/oauth2/device/code";
-    var clientId = "846766038767-8fs63le8h45dhjpf0umhc1ai07q4rhn7.apps.googleusercontent.com";
-    var scopes = "email profile";
+    var scopes = "email profile https://www.googleapis.com/auth/fitness.activity.read https://www.googleapis.com/auth/fitness.body.read";
 
-    request.post(url, { form: { client_id: clientId, scope: scopes } }, function(error, response, body) {
+    request.post(url, { form: { client_id: self.client_id, scope: scopes } }, function(error, response, body) {
       if (!error && response.statusCode == 200) {
         var data = JSON.parse(body);
-        self.device_code = data.device_code;
-        self.secret = data.client_secret;
+        self.config.device_code = data.device_code;
+        self.writeConfig();
 
-        self.sendSocketNotification("AUTH_BODY", data);
-        return;
+        self.tmpAuthData = data;
+
+        self.sendSocketNotification("AUTH_CODE_BODY", data);
+        self.getRefreshToken();
       } else {
-        self.sendSocketNotification("AUTH_ERROR", error);
+        self.sendSocketNotification("AUTH_CODE_ERROR", response);
       }
     });
   },
 
-  getOAuth: function () {
+  getRefreshToken: function () {
     var self = this;
-
-    self.oauth = true;
-
     var url = "https://www.googleapis.com/oauth2/v4/token";
-    var clientId = "846766038767-8fs63le8h45dhjpf0umhc1ai07q4rhn7.apps.googleusercontent.com";
-    var clientSecret = "kbJlTr7uQPCiClb5JSrGE-Ve";
-    var grantType = "http://oauth.net/grant_type/device/1.0";
+    var grant = "http://oauth.net/grant_type/device/1.0";
 
-    request.post(url, { form: { client_id: clientId, client_secret: clientSecret, code: self.device_code, grant_type: grantType } }, function (error, response, body) {
+    request.post(url, { form: { client_id: self.client_id, client_secret: self.client_secret, code: self.config.device_code, grant_type: grant } }, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        self.sendSocketNotification("OAUTH_BODY", JSON.parse(body));
-      } else {
-        self.sendSocketNotification("OAUTH_ERROR", error);
-        self.sendSocketNotification("OAUTH_ERROR", response);
-        self.sendSocketNotification("OAUTH_ERROR", body);
+        var data = JSON.parse(body);
+        self.sendSocketNotification("REFRESH_TOKEN_BODY", data);
 
-        setTimeout(function() { self.getOAuth() }, 10000);
+        self.config.refresh_token = data.refresh_token;
+        self.writeConfig();
+
+        self.getAccessToken();
+      } else {
+        self.sendSocketNotification("REFRESH_TOKEN_ERROR", response);
+        setTimeout(function() { self.getRefreshToken() }, 10000);
+      }
+    });
+  },
+
+  getAccessToken: function() {
+    var self = this;
+    var url = "https://www.googleapis.com/oauth2/v4/token";
+    var grant = "refresh_token";
+
+    request.post(url, { form: { client_id: self.client_id, client_secret: self.client_secret, refresh_token: self.config.refresh_token, grant_type: grant } }, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        var data = JSON.parse(body);
+        self.sendSocketNotification("ACCESS_TOKEN_BODY", data);
+        self.tmpAccessToken = data;
+
+        self.getStats();
+      } else {
+        self.sendSocketNotification("ACCESS_TOKEN_ERROR", response);
       }
     });
   },
@@ -57,26 +93,68 @@ module.exports = NodeHelper.create({
   getStats: function (url) {
     var self = this;
 
-    request({ url: url, method: "GET" }, function (error, response, body) {
+    var url = "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate";
+
+    var startTime = new Date();
+    startTime.setHours(0, 0, 0, 0);
+    startTime.setDate(startTime.getDate() - 6); // go seven days back
+
+    var endTime = new Date();
+    endTime.setHours(23, 59, 59, 999);
+
+    var req = {
+      "aggregateBy": [
+        {
+          "dataTypeName": "com.google.step_count.delta",
+          "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+        },
+        {
+          "dataTypeName": "com.google.weight",
+          "dataSourceId": "derived:com.google.weight:com.google.android.gms:merge_weight"
+        }
+      ],
+      "bucketByTime": { "durationMillis": 86400000 }, // 1 day per bucket
+      "startTimeMillis": startTime.getTime(),
+      "endTimeMillis": endTime.getTime()
+    };
+
+    var options = {
+      url: url,
+      json: req,
+      headers: {
+        Authorization: "Bearer " + self.tmpAccessToken.access_token
+      }
+    }
+
+    request.post(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        var result = JSON.parse(body);
-        self.sendSocketNotification("STATS_RESULT", result);
+        // body is already json at this point
+        self.sendSocketNotification("STATS", body);
+      } else {
+        self.sendSocketNotification("STATS_ERROR", error);
+        self.sendSocketNotification("STATS_ERROR", response);
+        self.sendSocketNotification("STATS_ERROR", body);
       }
     });
+  },
 
+  writeConfig: function() {
+    jsonfile.writeFileSync(dataFile, this.config, { spaces: 2 });
   },
 
   //Subclass socketNotificationReceived received.
   socketNotificationReceived: function(notification, payload) {
-    if (notification === "GET_STATS") {
-      this.getStats(payload);
-    } else if (notification === "GET_AUTH") {
-      if (!this.oauth) {
-        if (this.device_code) {
-          this.getOAuth();
-        } else {
-          this.getAuthCode();
-        }
+    if (notification === "UPDATE") {
+      if (this.config.refresh_token) {
+        this.sendSocketNotification("REFRESH_TOKEN_BODY", this.config.refresh_token);
+        this.getAccessToken(); // This will get an access token and then get stats afterwards if successful
+      } else if (this.tmpAuthData) {
+        // Just in case refreshing the page before auth complete
+        this.sendSocketNotification("AUTH_CODE_BODY", this.tmpAuthData);
+
+        this.getRefreshToken();
+      } else {
+        this.getAuthCode();
       }
     }
   }
